f00d457a87a631d9db96d441bc610897dce536f5
burcj3622 <burcj3622@wrdsb.ca>
2020-10-13 15:52:37 +0000

    grade

--- Main.java
+++ Main.java
@@ -1,15 +1,34 @@
+import java.util.Scanner;
 /**
  *
  * @author 
  */
 public class Main {
-
+  
+  public static void examgrade(int grade){
+  if (grade<50){System.out.println("F");}
+  if (grade>=50){System.out.println("D");}
+  if (grade<69){System.out.println("C");}
+  if (grade>70){System.out.println("B");}
+  if (grade>80){System.out.println("A");}
+  }
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    // asks user for input
+    System.out.println("Enter exam grade");
+    // gets user input
+    int grade = input.nextInt();
+    examgrade(grade);
+
+
   }
-}
+  
+  }
+
+
+
